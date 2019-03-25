5575a924104c0b01e06aa6e4b1d39a88d7fa7545
opsonly <13271331235@163.com>
2019-03-25 11:17:14 +0800

    test

--- commodity/models.py
+++ commodity/models.py
@@ -18,7 +18,7 @@ class commonDity(models.Model):
 
 class shopname(models.Model):
 
-    shop_name = models.CharField(max_length=100,default='null')
+    shop_name = models.CharField(max_length=100)
     common_id = models.IntegerField()
     commonNum = models.IntegerField()
     commonName = models.CharField(max_length=100,default='null')
